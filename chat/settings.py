4d588186c03c2659549e834d7ac84d04007e5425
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-11-06 21:03:30 +0100

    feat: add new allowed host.

--- chat/settings.py
+++ chat/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'chatapp.ChatUsers'
 
