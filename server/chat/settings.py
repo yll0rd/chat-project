bbb58d5382780090f625ad674779b55e1ceba48b
Leo-Youmbi <leoyoumbi83@gmail.com>
2024-01-21 18:10:15 +0100

    refactor

--- server/chat/settings.py
+++ server/chat/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = os.getenv("DEBUG", False)
 
 # ALLOWED_HOSTS = ['yll-chatapp.onrender.com', '127.0.0.1']
 ALLOWED_HOSTS = ['*']
